8152e0c4a62258145f8875da96a2cc450086b6b7
robex192 <62716821+robex192@users.noreply.github.com>
2021-03-13 00:39:55 +0300

    common files

--- index.php
+++ index.php
@@ -1,44 +1,86 @@
-<?php include 'header_login.php';?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login - <?php include('asset/includes/title.php'); ?></title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <!-- Bootstrap 3.3.5 -->
+        <link rel="stylesheet" href="asset/bootstrap/css/bootstrap.min.css">
 
-  <body class="login">
-    <div>
+        <!-- Font Awesome -->
+        <!-- Theme style -->
+        <link rel="stylesheet" href="asset/css/AdminLTE.min.css">
+        <!-- AdminLTE Skins. Choose a skin from the css/skins
+             folder instead of downloading all of them to reduce the load. -->
+        <link rel="stylesheet" href="asset/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" type="text/css" href=" asset/single_selector/chosen.min.css">
+        <link rel="stylesheet" type="text/css" href=" asset/single_selector/singlebox.css">		
     
+    </head>
+    <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+    <body class="hold-transition login-page">
 
-      <div class="login_wrapper">
-        <div class="animate form login_form">
-          <section class="login_content">
-            <form method = "POST" action = "login.php">
-              <h1>Administrator Login</h1>
-              <div>
-                <input type="text" name = "user_role" class="form-control" placeholder="user_role" required="true" />
-              </div>
-              <div>
-                <input type="password" name = "password" class="form-control" placeholder="Password" required="true" />
-              </div>
-              <div>
-                <button  class="btn btn-block btn-warning" name = "login"> Log in</button>
-              
-              </div>
+        <div class="login-box">
+            <div class="login-logo">
+                <b style="background-image: url(asset/images/kenema_log.jpg);">Kenema Online pharmacy Management system</b>
+            </div><!-- /.login-logo -->
+            <div class="login-box-body">
+                <p class="login-box-msg"></p>
+                <form action="login.php" method="post">
+                    <div class="form-group has-feedback">
+                        <input type="text" class="form-control" placeholder="User_role" name="user_role" required>
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" placeholder="Password" name="password" required>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <select class="singleChosen" style="width:100%" name="branch" required>
+						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            <?php
+                            include('asset/includes/dbcon.php');
 
-              <div class="clearfix"></div>
+                            $query3 = mysqli_query($con, "select * from branch order by branch_name")or die(mysqli_error($con));
+                            while ($row3 = mysqli_fetch_array($query3)) {
+                                ?>
+                                <option value="<?php echo $row3['branch_id']; ?>"><?php echo $row3['branch_name']; ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 pull-right">
+                            <button type="reset" class="btn btn-twitter btn-flat">Clear</button>
+                        </div><!-- /.col -->
+                        <div class="col-xs-6 pull-right">
+                            <button type="submit" class="btn btn-twitter btn-block btn-flat" name="login" default>Sign In</button>
+                        </div><!-- /.col -->
+                    </div>
+                </form>
 
-              <div class="separator">
-               
 
-                <div class="clearfix"></div>
-                <br />
 
-                <div>
-                  <h1><i class="fa fa-paw"></i> Kenema Online Pharmacy Management System </h1>
-                  <p>©2021 All Rights Reserved KOPMS </p>
-                </div>
-              </div>
-            </form>
-          </section>
-        </div>
+            </div><!-- /.login-box-body -->
+        </div><!-- /.login-box -->
 
-    
-      </div>
-    </div>
-  </body>
+
+
+        <!-- jQuery 2.1.4 -->
+        <script src="asset/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+        <!-- Bootstrap 3.3.5 -->
+        <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+        <!-- SlimScroll -->
+        <script src="asset/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+        <!-- FastClick -->
+        <script src="asset/plugins/fastclick/fastclick.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="asset/js/app.min.js"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="asset/js/demo.js"></script>
+        <script src="asset/single_selector/jquery.min.js"></script>
+        <script src="asset/single_selector/chosen.jquery.min.js"></script>
+        <script src="asset/single_selector/singlebox.js"></script>
+    </body>
 </html>
--- login.php
+++ login.php
@@ -1,38 +1,117 @@
-<?php session_start();
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+<link rel="stylesheet" href="asset/css/login.css">
+    </head>
+    <body>
+        <div id="cssload-pgloading">
+            <div class="cssload-loadingwrap">
+                <ul class="cssload-bokeh">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
 
-include('dbcon.php');
+    </body>
 
-if(isset($_POST['login']))
-{
+</html>
+<?php
+include('asset/includes/dbcon.php');
 
-$user_unsafe=$_POST['user_role'];
-$pass_unsafe=$_POST['password'];
+if (isset($_POST['login'])) {
+  	
+    $user_unsafe = $_POST['user_role'];
+    $pass_unsafe = $_POST['password'];
+    $branch = $_POST['branch'];
+   
+    $user = mysqli_real_escape_string($con, $user_unsafe);
+    $pass1 = mysqli_real_escape_string($con, $pass_unsafe);
 
-$user = mysqli_real_escape_string($con,$user_unsafe);
-$pass1 = mysqli_real_escape_string($con,$pass_unsafe);
-$pass=md5($pass1);
-$salt="a1Bz20ydqelm8m1wql";
-$pass=$salt.$pass;
+    $pass = md5($pass1);
+    $salt = "a1Bz20ydqelm8m1wql";
+    $pass = $salt . $pass;
+    date_default_timezone_set('Africa/Addis_ababa');
+    $date = date("Y-m-d H:i:s");
+    $query = mysqli_query($con, "select * from user natural join branch where user_role='$user' and password='$pass' and branch_id='$branch' and status='active'")or die(mysqli_error($con));
+    $row = mysqli_fetch_array($query);
+    $id = $row['user_id'];
+	$user_name = $row['user_name'];
+    $user_role = $row['user_role'];
+	
+    $counter = mysqli_num_rows($query);
 
-$query=mysqli_query($con,"select * from user where user_role='$user' and password='$pass' and status = 'Active' and branch_id='0'")or die(mysqli_error($con));
-		$row=mysqli_fetch_array($query);
-           $id=$row['user_id'];
-          /*  $first=$row['admin_first'];
-           $last=$row['admin_last']; */
-           $counter=mysqli_num_rows($query);
+    $id = $row['user_id'];
+	 $_SESSION['user_name'] = $row['user_id'];
+    $_SESSION['branch'] = $row['branch_id'];
+    $_SESSION['skin'] = $row['skin'];
 
-		  	if ($counter == 0) 
-			  {	
-				  echo "<script type='text/javascript'>alert('Invalid Username or Password!');
-				  document.location='index.php'</script>";
-			  } 
-			  else
-			  {
-				  $_SESSION['id']=$id;	
-				  $_SESSION['branch']=0;	
-				  /* $_SESSION['name']=$first." ".$last; */
+    if ($counter == 0) {
+        echo "<script type='text/javascript'>alert('Invalid Username or Password!');
+	  document.location='index.php'</script>";
+    }
+    elseif($user == "Data Clerk") {
+        $_SESSION['id'] = $id;
+        $_SESSION['user_role'] = $user_role;
+		$_SESSION['user_name'] = $user_name;
+        
+        $remarks = "has logged in the system at ";
+        mysqli_query($con, "INSERT INTO history_log(user_id,action,date) VALUES('$id','$remarks','$date')")or die(mysqli_error($con));
+        
+        echo "<script type='text/javascript'>document.location='data_clerk/home.php'</script>"; //header("location:pages/home.php");
+       
+    }
+    elseif ($user == "Pharmacist") {
+        $_SESSION['id'] = $id;
+        $_SESSION['user_role'] = $user_role;
+        $_SESSION['user_name'] = $user_name;
+        $remarks = "has logged in the system at ";
+        mysqli_query($con, "INSERT INTO history_log(user_id,action,date) VALUES('$id','$remarks','$date')")or die(mysqli_error($con));
+        echo "<script type='text/javascript'>document.location='pharmacist/home_pharmacist.php'</script>";
+    }
+   elseif($user == "Cashier") {
+        $_SESSION['id'] = $id;
+        $_SESSION['user_role'] = $user_role;
+        $_SESSION['user_name'] = $user_name;
+        $remarks = "has logged in the system at ";
+        mysqli_query($con, "INSERT INTO history_log(user_id,action,date) VALUES('$id','$remarks','$date')")or die(mysqli_error($con));
+        echo "<script type='text/javascript'>document.location='cashier/home_cashier.php'</script>";
+    }
+  elseif($user == "Pharmacy Manager") {
+        $_SESSION['id'] = $id;
+        $_SESSION['user_role'] = $user_role;
+           $_SESSION['user_name'] = $user_name;
+        $remarks = "has logged in the system at ";
+        mysqli_query($con, "INSERT INTO history_log(user_id,action,date) VALUES('$id','$remarks','$date')")or die(mysqli_error($con));
+        echo "<script type='text/javascript'>document.location='pharmacy_manager/home_pharmacy_manager.php'</script>";
+    }
+    elseif($user == "Stock Manager") {
+        $_SESSION['id'] = $id;
+        $_SESSION['user_role'] = $user_role;
+         $_SESSION['user_name'] = $user_name;
+        $remarks = "has logged in the system at ";
+        mysqli_query($con, "INSERT INTO history_log(user_id,action,date) VALUES('$id','$remarks','$date')")or die(mysqli_error($con));
 
-			  	echo "<script type='text/javascript'>document.location='dashboard_admin.php'</script>";  
-			  }
+        echo "<script type='text/javascript'>document.location='stock_manager/home_stock_manager.php'</script>";
+    }
+	    elseif($user != $user_role) {
+        $_SESSION['id'] = $id;
+        $_SESSION['user_role'] = $user_role;
+         $_SESSION['user_name'] = $user_name;
+        $remarks = "has logged in the system at ";
+        mysqli_query($con, "INSERT INTO history_log(user_id,action,date) VALUES('$id','$remarks','$date')")or die(mysqli_error($con));
+
+        echo "<script type='text/javascript'>alert('user role is inccorrect!')document.location='index.php'</script>";
+    }
+}
+else {
+echo "<script type='text/javascript'>alert('No Such kind of account!');document.location='index.php'</script>";	
 }
 ?>
+
+
